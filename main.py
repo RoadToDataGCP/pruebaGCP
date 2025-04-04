fb38c4802d05f58357b9dc0fc137f7ae58b8addb
RoadToDataGCP <road.to.data.gcp@gmail.com>
2025-04-04 13:30:07 +0200

    Cambio

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "¡Hola desde Cloud Run conectado por GitHub!"
+    return "¡He cambiado!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
